ab04d9992dfd3d7b7530df14fb3abd7a0fa85f73
hemel-cse <hasibul.amin.hemel@gmail.com>
2018-09-22 01:48:01 +0600

    android

--- android/app/src/main/java/com/sparkgroupbd.ipcapp/MainActivity.java
+++ android/app/src/main/java/com/sparkgroupbd.ipcapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ipcapp;
+package com.sparkgroupbd.ipcapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "IPCAPP";
+        return "SPARKGROUPBD.IPCAPP";
     }
 }
